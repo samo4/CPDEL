edf323cdec607f9659c90468b75a06c88b729531
Samo 4 <samo4@users.noreply.github.com>
2024-11-05 12:04:04 +0100

    css

--- firmware_playground/spa/assets/scripts/components/Card.js
+++ firmware_playground/spa/assets/scripts/components/Card.js
@@ -9,18 +9,24 @@ export default Card = {
   template: `
   <div class="card" v-if="card.type=='generic'" ref="cardEl">
     <i>📋</i>
-    <h5>{{ card.name }}</h5>
-    <p>{{ card.value }} <small>{{ card.symbol }}</small></p>
+    <div class="text-group">
+      <h5>{{ card.name }}</h5>
+      <p>{{ card.value }} <small>{{ card.symbol }}</small></p>
+    </div>
   </div>
   <div class="card" v-else-if="card.type=='temperature'">
     <i>🌡️</i>
-    <h5>{{ card.name }}</h5>
-    <p>{{ card.value }} <small>{{ card.symbol }}</small></p>
+    <div class="text-group">
+      <h5>{{ card.name }}</h5>
+      <p>{{ card.value }} <small>{{ card.symbol }}</small></p>
+    </div>
   </div>
   <div class="card" v-else-if="card.type=='humidity'">
     <i>💧</i>
-    <h5>{{ card.name }}</h5>
-    <p>{{ card.value }} <small>{{ card.symbol }}</small></p>
+    <div class="text-group">
+      <h5>{{ card.name }}</h5>
+      <p>{{ card.value }} <small>{{ card.symbol }}</small></p>
+    </div>
   </div>
   <div class="card" v-else-if="card.type=='status'">
     <i v-if="card.symbol==='i'">💤</i>
@@ -28,31 +34,41 @@ export default Card = {
     <i v-else-if="card.symbol==='s'">✅</i>
     <i v-else-if="card.symbol==='d'">⚠️</i>
     <i v-else>❓</i>
-    <h5>{{ card.name }}</h5>
-    <p>{{ card.value }}</p>
+    <div class="text-group">
+      <h5>{{ card.name }}</h5>
+      <p>{{ card.value }}</p>
+    </div>
   </div>
   <div class="card" v-else-if="card.type=='progress'">
     <i>📊</i>
-    <h5>{{ card.name }}</h5>
-    <p>
-      <progress :value="card.value" max="100"></progress>
-      <small>{{ card.value }}%</small>
-    </p>
+    <div class="text-group">
+      <h5>{{ card.name }}</h5>
+      <p>
+        <progress :value="card.value" max="100"></progress>
+        <small>{{ card.value }}%</small>
+      </p>
+    </div>
   </div>
   <div class="card" v-else-if="card.type=='slider'">
     <i>🎚️</i>
-    <h5>{{ card.name }}</h5>
-    <p>
-      <input type="range" v-model="card.value" :min="card.min" :max="card.max" @change="changeSlider" />
-    </p>
+    <div class="text-group">
+      <h5>{{ card.name }}</h5>
+      <p>
+        <input type="range" v-model="card.value" :min="card.min" :max="card.max" @change="changeSlider" />
+      </p>
+    </div>
   </div>
   <div class="card" v-else-if="card.type=='button'">
     <i>🔘</i>
-    <p><button @click="clickButton">{{ card.name }}</button></p>
+    <div class="text-group">
+      <p><button @click="clickButton">{{ card.name }}</button></p>
+    </div>
   </div>
   <div class="card appendable" v-else-if="card.type=='appendable'" ref="cardEl">
-    <h5>{{ card.name }}</h5>
-    <textarea ref="textareaEl" disabled rows=10>{{ card.value }}</textarea>
+    <div class="text-group">
+      <h5>{{ card.name }}</h5>
+      <textarea ref="textareaEl" disabled rows=10>{{ card.value }}</textarea>
+    </div>
   </div>
   <div class="card chart" v-else-if="card.type=='bar'" ref="cardEl">
     <h5>{{ card.name }}</h5>
@@ -60,12 +76,16 @@ export default Card = {
   </div>
   <div class="card chart" v-else-if="card.type=='line'" ref="cardEl">
     <h5>{{ card.name }}</h5>
-    <Line :x="card.x" :y="card.y" />
+    <div class="text-group">
+      <Line :x="card.x" :y="card.y" />
+    </div>
   </div>
   <div class="card" v-else>
     <i>❓</i>
-    <h5>Unknown card</h5>
-    <p>{{ card }}</p>
+    <div class="text-group">
+      <h5>Unknown card</h5>
+      <p>{{ card }}</p>
+    </div>
   </div>
   `,
   props: {

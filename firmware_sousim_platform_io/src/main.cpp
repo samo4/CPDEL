4b4cd150cc8165f629a34d704a113914d4c13691
Samo 4 <samo4@users.noreply.github.com>
2025-05-18 15:49:04 +0200

    more default

--- firmware_sousim_platform_io/src/main.cpp
+++ firmware_sousim_platform_io/src/main.cpp
@@ -27,6 +27,11 @@ WiFiClient network;
 
 AsyncWebServer server(80);
 ESPDash dashboard(&server);
+Card cardDateTime(&dashboard, GENERIC_CARD, "Datetime");
+Card cardUptime(&dashboard, GENERIC_CARD, "Uptime");
+Card cardLastResetReason(&dashboard, GENERIC_CARD, "Last reset reason");
+Card cardIpV6(&dashboard, GENERIC_CARD, "ipv6");
+Card cardVersion(&dashboard, GENERIC_CARD, "Version");
 
 static struct tm timeinfo;
 const int CHART_SIZE = 24;
@@ -34,6 +39,7 @@ float YAxis[CHART_SIZE] = {0.0};
 int XAxis[CHART_SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
 
 String resetReason;
+long lastExecTime1 = -1000000;
 constexpr int WDT_TIMEOUT_S = 3 * 60 * 60;
 constexpr size_t OUTPUT_SIZE = 32;
 constexpr long DELAY_BY_MS = 90 * 60 * 1000;
@@ -78,4 +84,31 @@ void setup() {
   dashboard.sendUpdates();
 }
 
-void loop() {}
+void loop() {
+  long currentTime = millis();
+  wifiManager.handle();
+  ElegantOTA.loop();
+
+  if (currentTime - lastExecTime1 >= 60 * 1000) {
+    printLocalTime();
+    lastExecTime1 = currentTime;
+
+    cardUptime.update(String(currentTime / (1000 * 60 * 60)) + "h ");
+    cardLastResetReason.update(resetReason);
+    cardIpV6.update(SimpleWifiManager::hasIpV6() ? "Yes" : "No");
+    cardVersion.update(GIT_HASH);
+    dashboard.sendUpdates();
+  }
+}
+
+void printLocalTime() {
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    cardDateTime.update("Failed to obtain time");
+  } else {
+    static char s[51];
+    strftime(s, 50, "%b %d %H:%M", &timeinfo);
+    cardDateTime.update(String(s));
+    Serial.println(s);
+  }
+}
